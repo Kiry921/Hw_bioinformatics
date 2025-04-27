bd90c1484f0efc4e6c8b62a856bceec64dd4ca7e
Kirill <134440347+Kiry921@users.noreply.github.com>
2025-04-27 17:32:55 +0300

    Add files via upload

---
+++ Hw_10/Trim_light/genome_trimm.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#SBATCH --job-name=trimming     # Job name
+#SBATCH --cpus-per-task=4         # Run on a single CPU
+#SBATCH --mem=16gb                 # Job memory request
+#SBATCH --time=1:30:00           # Time limit hrs:min:sec
+#SBATCH --output=JobName.%j.Log  # Standard output and error log
+#SBATCH --partition=short
+
+source activate trimming
+
+trimmomatic PE /projects/mipt_dbmp_biotechnology/genome/illumina_reads_R1_001.fastq \
+/projects/mipt_dbmp_biotechnology/genome/illumina_reads_R2_001.fastq \
+-baseout ~/classes/class_10/trimmed/illumina_reads_trim4.fastq \
+-trimlog ~/classes/class_10/trimmed/illumina_reads_trim4.fastq \
+ILLUMINACLIP:/projects/mipt_dbmp_biotechnology/genome/adapters.fa:2:30:10 SLIDINGWINDOW:4:20 MAXINFO:50:0.2 LEADING:20 TRAILING:20 MINLEN:36
